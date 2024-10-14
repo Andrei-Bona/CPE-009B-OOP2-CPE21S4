a66d173ffae0bd825090ae5ba5c6d14fd4e2d6a8
Andrei-Bona <149856450+Andrei-Bona@users.noreply.github.com>
2024-10-14 10:16:42 +0800

    Add files via upload

---
+++ Laboratory Activity 4 - Introduction to GUI Development using Pycharm/Supplementary Activity/registration.py
@@ -0,0 +1,98 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QIcon, QFont
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__() #initializes the main window like in the previous one
+        #window = QMainWindow()
+        self.title = "Account Register System"
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(200,200,400,500)
+        self.setWindowIcon(QIcon('pythonico.ico')) #sets an icon
+
+
+        self.MainLabel = QLabel('Account Register System', self)
+        self.MainLabel.setFont(QFont('Times New Roman', 10))
+        self.MainLabel.setStyleSheet("color: blue;")
+        self.MainLabel.move(130, 20)
+
+        self.firstname = QLabel('First Name: ', self)
+        self.firstname.setFont(QFont('Times New Roman', 10))
+        self.firstname.move(50, 70)
+        self.firstname = QLineEdit(self)
+        self.firstname.move(125, 55)
+        self.firstname.resize(200, 40)
+
+        self.lastname = QLabel('Last Name: ', self)
+        self.lastname.setFont(QFont('Times New Roman', 10))
+        self.lastname.move(50, 120)
+        self.lastname = QLineEdit(self)
+        self.lastname.move(125, 105)
+        self.lastname.resize(200, 40)
+
+        self.username = QLabel('Username: ', self)
+        self.username.setFont(QFont('Times New Roman', 10))
+        self.username.move(50, 170)
+        self.username = QLineEdit(self)
+        self.username.move(125, 155)
+        self.username.resize(200, 40)
+
+        self.password = QLabel('Password: ', self)
+        self.password.setFont(QFont('Times New Roman', 10))
+        self.password.move(50, 220)
+        self.password = QLineEdit(self)
+        self.password.move(125, 205)
+        self.password.resize(200, 40)
+
+        self.email = QLabel('Email Address: ', self)
+        self.email.setFont(QFont('Times New Roman', 10))
+        self.email.move(50, 270)
+        self.email = QLineEdit(self)
+        self.email.move(125, 255)
+        self.email.resize(200, 40)
+
+        self.con_number = QLabel('Contact\nNumber: ', self)
+        self.con_number.setFont(QFont('Times New Roman', 10))
+        self.con_number.move(50, 310)
+        self.con_number = QLineEdit(self)
+        self.con_number.move(125, 305)
+        self.con_number.resize(200, 40)
+
+
+        self.submit = QPushButton('Submit', self)
+        self.submit.setToolTip("Done?")
+        self.submit.move(125, 400) # button.move(x,y)
+        self.submit.clicked.connect(self.submit_input)
+
+        clear = QPushButton('Clear', self)
+        clear.setToolTip("Reset?")
+        clear.move(250, 400) # button.move(x,y)
+        clear.clicked.connect(self.clear_input)
+
+
+
+        self.show()
+
+    def submit_input(self):
+        first_name = self.firstname.text()
+        last_name = self.lastname.text()
+        username = self.username.text()
+        password = self.password.text()
+        email = self.email.text()
+        contact_number = self.con_number.text()
+
+        message = f"Submitted:\nFirst Name: {first_name}\nLast Name: {last_name}\nUsername: {username}\nEmail: {email}\nContact Number: {contact_number}"
+        QMessageBox.information(self, "Submission", message)
+
+    def clear_input(self):
+        self.firstname.clear()
+        self.lastname.clear()
+        self.username.clear()
+        self.password.clear()
+        self.email.clear()
+        self.con_number.clear()
---
+++ Laboratory Activity 4 - Introduction to GUI Development using Pycharm/gui_labels.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLineEdit, QLabel
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__() #initializes the main window like in the previos one
+        # window = QMainWindow()
+        self.title = "PyQt Line Edit"
+        self.x = 200 # or left
+        self.y = 200 # or top
+        self.width = 300
+        self.height = 300
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.x, self.y, self.width, self.height)
+        self.setWindowIcon(QIcon('pythonico.ico'))
+
+        self.textboxbl = QLabel("Hello World", self)
+        self.textboxbl.move(120, 100)
+        self.textboxbl2 = QLabel("This program is written in Pycharm", self)
+        self.textboxbl2.move(70, 120)
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
